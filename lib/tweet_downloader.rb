2c8fb7c3384b0a35821db6a2d736e9791fbf45c9
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2012-02-11 21:38:37 +0100

    download the referred tweet from replies
    yes, this is messy

--- lib/tweet_downloader.rb
+++ lib/tweet_downloader.rb
@@ -8,17 +8,24 @@ class TweetDownloader
   attr_reader :user, :tweet_dir
 
   # twitter wants 150 requests per hour at max, we're being nice, so we sleep about 30 seconds before doing a new request
-  SLEEPY_TIME = 3600 / 120
+  SLEEPY_TIME = 1 # 3600 / 120
+  MAX_REQUESTS = 150
 
   def initialize(username, directory)
     raise "username is incorrect: #{username.inspect}" if username.nil? or username.empty?
     @user = username
     @tweet_dir = directory
+    @requests = 0
   end
 
   # I just realized that I don't know for sure if status ids are increasing...
   def since_id
     Dir.glob("#{tweet_dir}/*.json").map { |f| File.basename(f, ".json").to_i rescue 1 }.max
+  end
+
+  def go(options = {})
+    download_tweets(options)
+    download_in_reply_to(options)
   end
 
   def download_tweets(options = {})
@@ -28,14 +35,17 @@ class TweetDownloader
     STDERR.puts "#{tweets.size} tweets"
     save_tweets(tweets)
     unless tweets.empty?
-      sleep (SLEEPY_TIME)
-      # twitter starts counting pages at 1 (not a zero)
+      # twitter starts counting pages at 1 (not at zero)
       download_tweets(options.merge(:page => (options[:page] || 1) + 1))
     end
   end
 
   def filename(tweet)
-    "#{tweet_dir}/#{tweet["id"]}.json"
+    filename_for_id(tweet["id"])
+  end
+
+  def filename_for_id(id)
+    "#{tweet_dir}/#{id}.json"
   end
 
   def save_tweets(tweets)
@@ -60,6 +70,17 @@ class TweetDownloader
     end
   end
 
+  def show_path(options = {})
+    path = "/1/statuses/show.json"
+    unless options.empty?
+      opts = options.map do |k,v|
+        next if v.nil?
+        "#{k}=#{v}"
+      end
+      "#{path}?#{opts.join("&")}"
+    end
+  end
+
   def fetch_page(options = {})
     STDERR.puts "Fetching page... #{options.inspect}"
     json = ""
@@ -68,7 +89,38 @@ class TweetDownloader
       response = http.get(page_path(options))
       json = JSON.parse(response.body)
     end
+    @requests += 1
+    json
+  end
+
+  def download_in_reply_to(options = {})
+    return if @requests >= MAX_REQUESTS
+    count = 0
+    Dir.glob("#{tweet_dir}/*").each do |file|
+      tweet = JSON.parse(File.read(file))
+      if id = tweet["in_reply_to_status_id"]
+        unless File.exist?(filename_for_id(id))
+          tweet = fetch_one_tweet(id)
+          return count if tweet.nil?
+          save_tweets [tweet]
+          sleep(SLEEPY_TIME)
+          count += 1
+        end
+      end      
+    end
+    return count + fetch_in_reply_to(options) unless count == 0
+  end
+
+  def fetch_one_tweet(id)
+    return if @requests >= MAX_REQUESTS
+    STDERR.puts "Fetching tweet #{id}..."
+    json = ""
+    req = Net::HTTP.start('api.twitter.com') do |http|
+      http.read_timeout = 600
+      response = http.get(show_path(:id => id, :include_entities => true))
+      json = JSON.parse(response.body)
+    end
+    @requests += 1
     json
   end
 end
-
