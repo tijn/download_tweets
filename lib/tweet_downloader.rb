ffe5ea2d36c32cab7c6a317396c4edc9603b06c9
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2012-02-19 17:10:14 +0100

    remove the code I was most ashamed of

--- lib/tweet_downloader.rb
+++ lib/tweet_downloader.rb
@@ -2,25 +2,25 @@
 require 'rubygems'
 require 'net/http'
 require 'json'
+require 'twitter'
 
 
 class TweetDownloader
   attr_reader :user, :tweet_dir
 
-  # twitter wants 150 requests per hour at max, we're being nice, so we sleep about 30 seconds before doing a new request
-  SLEEPY_TIME = 1 # 3600 / 120
-  MAX_REQUESTS = 150
-
   def initialize(username, directory)
     raise "username is incorrect: #{username.inspect}" if username.nil? or username.empty?
     @user = username
     @tweet_dir = directory
-    @requests_remaining = MAX_REQUESTS
   end
 
   # I just realized that I don't know for sure if status ids are increasing...
-  def since_id
+  def latest_downloaded
     Dir.glob("#{tweet_dir}/*.json").map { |f| File.basename(f, ".json").to_i rescue 1 }.max
+  end
+
+  def fails
+    @fails ||= Dir.glob("#{tweet_dir}/*.fail").map { |f| File.basename(f, ".fail").to_i }
   end
 
   def go(options = {})
@@ -29,30 +29,34 @@ class TweetDownloader
   end
 
   def download_tweets(options = {})
-    options[:since_id] ||= since_id
+    options[:since_id] ||= latest_downloaded
     options[:count] ||= 100
     tweets = fetch_page(options)
     STDERR.puts "#{tweets.size} tweets"
-    save_tweets(tweets)
+    save_tweet(*tweets)
     unless tweets.empty?
       # twitter starts counting pages at 1 (not at zero)
       download_tweets(options.merge(:page => (options[:page] || 1) + 1))
     end
   end
 
-  def filename(tweet)
-    filename_for_id(tweet["id"])
+  def filename(status)
+    filename_for_id(status.id)
   end
 
   def filename_for_id(id)
     "#{tweet_dir}/#{id}.json"
   end
 
-  def save_tweets(tweets)
-    tweets.each do |tweet|
-      next if File.exists?(filename(tweet))
-      File.open(filename(tweet), 'w') { |f| f.puts tweet.to_json }
-      STDERR.puts "#{tweet["created_at"]}\t#{tweet["text"]}"
+  def filename_for_fail(id)
+    "#{tweet_dir}/#{id}.fail"
+  end
+
+  def save_tweet(*tweets)
+    tweets.each do |status|
+      next if File.exists?(filename(status))
+      File.open(filename(status), 'w') { |f| f.puts status.attrs.to_json }
+      STDERR.puts "#{status.created_at}\t#{status.user.screen_name}: #{status.text}"
     end
   rescue
     STDERR.puts tweets.inspect
@@ -70,59 +74,35 @@ class TweetDownloader
     end
   end
 
-  def show_path(options = {})
-    path = "/1/statuses/show.json"
-    unless options.empty?
-      opts = options.map do |k,v|
-        next if v.nil?
-        "#{k}=#{v}"
-      end
-      "#{path}?#{opts.join("&")}"
-    end
-  end
-
   def fetch_page(options = {})
-    return if @requests_remaining == 0
-    STDERR.puts "Fetching page... #{options.inspect}"
-    json = ""
-    req = Net::HTTP.start('twitter.com') do |http|
-      http.read_timeout = 600
-      response = http.get(page_path(options))
-      @requests_remaining = response["x-ratelimit-remaining"].to_i
-      json = JSON.parse(response.body)
+    Twitter.user_timeline(user, options).each do |status|
+      yield status if block_given?
     end
-    json
   end
 
   def download_in_reply_to(options = {})
-    return if @requests_remaining == 0
     count = 0
-    Dir.glob("#{tweet_dir}/*").each do |file|
+    Dir.glob("#{tweet_dir}/*.json").each do |file|
       tweet = JSON.parse(File.read(file))
       if id = tweet["in_reply_to_status_id"]
-        unless File.exist?(filename_for_id(id))
-          tweet = fetch_one_tweet(id)
-          return count if tweet.nil?
-          save_tweets [tweet]
-          sleep(SLEEPY_TIME)
-          count += 1
-        end
+        next if File.exist?(filename_for_id(id)) 
+        next if fails.include?(id)
+        status = fetch_one_tweet(id)
+        next if status.nil?
+        save_tweet status
+        count += 1
       end      
     end
-    return count + download_in_reply_to(options) unless count == 0
+    count
   end
 
   def fetch_one_tweet(id)
-    puts "@requests_remaining " + @requests_remaining.to_s
-    return if @requests_remaining == 0
-    STDERR.puts "Fetching tweet #{id}..."
-    json = ""
-    req = Net::HTTP.start('api.twitter.com') do |http|
-      http.read_timeout = 600
-      response = http.get(show_path(:id => id, :include_entities => true))
-      @requests_remaining = response["x-ratelimit-remaining"].to_i
-      json = JSON.parse(response.body)
-    end
-    json
+    Twitter.status(id)
+  rescue Twitter::Error::NotFound => error
+    File.open(filename_for_fail(id), 'w') { |f| f.puts error }
+    return nil
+  rescue
+    STDERR.puts "error downloading #{id}", $!
+    raise
   end
 end
